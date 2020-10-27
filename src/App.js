99880f5ff5622d149b3bc9ce1494ed6da7d79cf1
Dmitriy <shalberkind@mail.ru>
2020-10-27 14:31:33 +0300

    Работа с объектом props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Fcomponent from "./Fcomponent";
+import Ccomponent from "./Ccomponent";
+import { Menu } from "./Menu";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {/* <Fcomponent customField="Custom Fcomponent Field" /> */}
+        <Fcomponent />
+        <Menu />
+        <Ccomponent />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
--- src/Fcomponent.js
+++ src/Fcomponent.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-export default function Fcomponent() {
+
+export default function Fcomponent(props) {
   return (
     <div>
-      <h1>Functional Component</h1>
+      <h1>Functional Component {props.customField}</h1>
     </div>
   );
 }
+
+Fcomponent.defaultProps = { customField: "Defalut field" };
