c0b71ed83fdca31747bd2e0c0e6f722f82aedb6a
Dmitriy <shalberkind@mail.ru>
2020-10-27 19:09:25 +0300

    Работа с объектом state

--- src/Ccomponent.js
+++ src/Ccomponent.js
@@ -1,10 +1,38 @@
 import React, { Component } from "react";
 
 export default class Ccomponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+    };
+
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+  increment() {
+    this.setState((state) => ({
+      count: ++state.count,
+    }));
+  }
+  decrement() {
+    this.setState((state) => ({
+      count: --state.count,
+    }));
+  }
+  reset() {
+    this.setState({ count: 0 });
+  }
+
   render() {
     return (
       <div>
-        <h1>Class Component</h1>
+        <h1>Current value: {this.state.count}</h1>
+        <button onClick={this.increment}>increment</button>
+        <button onClick={this.decrement}>decrement</button>
+        <button onClick={this.reset}>reset</button>
       </div>
     );
   }
