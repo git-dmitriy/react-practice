eea7ef6e0745d6cee1d7be3a4a93e3acfbe373d0
Dmitriy <shalberkind@mail.ru>
2020-10-30 23:21:01 +0300

    Вывод писка вопросов

--- src/components/ActiveQuiz/ActiveQuiz.js
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -1,7 +1,8 @@
 import React from "react";
 import classes from "./ActiveQuiz.module.css";
+import AnswersList from "./AnswersList/AnswersList";
 
-const ActiveQuiz = () => {
+const ActiveQuiz = (props) => {
   return (
     <div className={classes.ActiveQuiz}>
       <p className={classes.Question}>
@@ -10,13 +11,7 @@ const ActiveQuiz = () => {
         </span>
         <small>1 из 12</small>
       </p>
-      <ul className="list-group">
-        <li className="list-group-item">First</li>
-        <li className="list-group-item">Second</li>
-        <li className="list-group-item">Third</li>
-        <li className="list-group-item">Fourh</li>
-        <li className="list-group-item">Fifth</li>
-      </ul>
+      <AnswersList answers={props.answers} />
     </div>
   );
 };
