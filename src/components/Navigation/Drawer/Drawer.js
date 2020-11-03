8d6ec4039dbe9a6e5ca144e44d4c58433e389500
Dmitriy <shalberkind@mail.ru>
2020-11-03 18:46:59 +0300

    Ссылки для навигации

--- src/components/Navigation/Drawer/Drawer.js
+++ src/components/Navigation/Drawer/Drawer.js
@@ -1,15 +1,39 @@
 import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 import Backdrop from "../../UI/Backdrop/Backdrop";
 import classes from "./Drawer.module.css";
 
-const links = [1, 2, 3];
+const links = [
+  {
+    to: "/",
+    label: "Список",
+    exact: true,
+  },
+  {
+    to: "/auth",
+    label: "Авторизация",
+    exact: true,
+  },
+  {
+    to: "/quiz-creactor",
+    label: "Создать тест",
+    exact: true,
+  },
+];
 
 class Drawer extends Component {
   renderLinks() {
     return links.map((link, index) => {
       return (
         <li key={index}>
-          <a>Link {link}</a>
+          <NavLink
+            to={link.to}
+            exact={link.exact}
+            activeClassName={classes.active}
+            onClick={this.props.onClose}
+          >
+            {link.label}
+          </NavLink>
         </li>
       );
     });
