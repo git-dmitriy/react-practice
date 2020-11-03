35e3b8cfc2f36efae9ec3d2137e690c5115d4e15
Dmitriy <shalberkind@mail.ru>
2020-11-03 17:08:11 +0300

    Создание страниц

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
 import Layout from "./hoc/Layout/Layout";
 import Quiz from "./containers/Quiz/Quiz";
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Auth from "./containers/Auth/Auth";
+import QuizCreator from "./containers/QuizCreator/QuizCreator";
+import QuizList from "./containers/QuizList/QuizList";
 
 function App() {
   return (
-    <Layout>
-      <Quiz />
-
-    </Layout>
+    <Router>
+      <Layout>
+        <Switch>
+          <Route path="/auth" component={Auth} />
+          <Route path="/quiz-creator" component={QuizCreator} />
+          <Route path="/quiz/:id" component={Quiz} />
+          <Route path="/" component={QuizList} />
+        </Switch>
+      </Layout>
+    </Router>
   );
 }
 
