d40b87450f9673037763c4fc0fefa43f5c1e2687
Dmitriy <shalberkind@mail.ru>
2020-11-04 16:17:10 +0300

    Добавлен компонент текстового поля формы

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import classes from "./Auth.module.css";
 import Button from "../../components/UI/Button/Button";
+import Input from "../../components/UI/Input/Input";
 
 export default class Auth extends Component {
   logInHandler = () => {};
@@ -16,8 +17,9 @@ export default class Auth extends Component {
           <h1>Авторизация</h1>
 
           <form onSubmit={this.submitHandler} className={classes.AuthForm}>
-            <input type="text" />
-            <input type="text" />
+            <Input label="email" />
+            <Input label="Пароль" errorMessage="Error" />
+
             <Button type="success" onClick={this.logInHandler}>
               Войти
             </Button>
