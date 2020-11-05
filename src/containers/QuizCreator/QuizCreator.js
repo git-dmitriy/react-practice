69a460f59c67d3792d7b4f771fa76cf76db22eb3
Dmitriy <shalberkind@mail.ru>
2020-11-05 14:18:38 +0300

    Оформление страницы создания тестов

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -1,10 +1,42 @@
 import React, { Component } from "react";
+import classes from "./QuizCreator.module.css";
+import Button from "../../components/UI/Button/Button";
 
 export default class QuizCreator extends Component {
+  submitHandler = (event) => {
+    event.preventDefault();
+  };
+
+  addQuestionHandler = () => {};
+  createQuizHandler = () => {};
+
   render() {
     return (
-      <div>
-        <h1>Quiz Creator</h1>
+      <div className={classes.QuizCreator}>
+        <diV>
+          <h1>Создание теста</h1>
+
+          <form
+            onSubmit={this.submitHandler}
+            className={classes.QuizCreatorForm}
+          >
+            <input type="text" />
+            <hr />
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+
+            <select></select>
+
+            <Button type="primary" onClick={this.addQuestionHandler}>
+              Добавить вопрос
+            </Button>
+            <Button type="success" onclick={this.createQuizHandler}>
+              Создать тест
+            </Button>
+          </form>
+        </diV>
       </div>
     );
   }
