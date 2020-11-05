0abb8b0d59017307643c24dc0ca8843b0dccaf01
Dmitriy <shalberkind@mail.ru>
2020-11-05 22:58:31 +0300

    Подключен компонент select

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -3,6 +3,7 @@ import classes from "./QuizCreator.module.css";
 import Button from "../../components/UI/Button/Button";
 import { createControl } from "../../Utilities/form";
 import Input from "../../components/UI/Input/Input";
+import Select from "../../components/UI/Select/Select";
 
 function createOptionControl(num) {
   return createControl(
@@ -34,6 +35,7 @@ function createFormControls() {
 export default class QuizCreator extends Component {
   state = {
     quiz: [],
+    rightAnswerId: 1,
     formControls: createFormControls(),
   };
 
@@ -68,7 +70,28 @@ export default class QuizCreator extends Component {
     });
   }
 
+  selectChangeHandler = (event) => {
+    this.setState({
+      rightAnswerId: +event.target.value,
+    });
+    console.log(event.target.value);
+  };
+
   render() {
+    const select = (
+      <Select
+        label="Выберите правильный ответ"
+        value={this.state.rightAnswerId}
+        onChange={this.selectChangeHandler}
+        options={[
+          { text: 1, value: 1 },
+          { text: 2, value: 2 },
+          { text: 3, value: 3 },
+          { text: 4, value: 4 },
+        ]}
+      />
+    );
+
     return (
       <div className={classes.QuizCreator}>
         <div>
@@ -80,7 +103,7 @@ export default class QuizCreator extends Component {
           >
             {this.renderInputs()}
 
-            <select></select>
+            {select}
 
             <Button type="primary" onClick={this.addQuestionHandler}>
               Добавить вопрос
