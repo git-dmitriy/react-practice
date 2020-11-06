c33f76d9f46cf7a5ddbc0f1e29bf3b3b344903e1
Dmitriy <shalberkind@mail.ru>
2020-11-06 20:01:41 +0300

    Добавлен модуль основной адрес для запросов axio

---
+++ src/axios/axios-quiz.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "https://react-tests-30d50.firebaseio.com/",
+});
--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -4,7 +4,7 @@ import Button from "../../components/UI/Button/Button";
 import { createControl, validate, validateForm } from "../../Utilities/form";
 import Input from "../../components/UI/Input/Input";
 import Select from "../../components/UI/Select/Select";
-import axios from "axios";
+import axios from "../../axios/axios-quiz";
 
 function createOptionControl(num) {
   return createControl(
@@ -83,10 +83,7 @@ export default class QuizCreator extends Component {
     event.preventDefault();
 
     try {
-      await axios.post(
-        "https://react-tests-30d50.firebaseio.com/quizes.json",
-        this.state.quiz
-      );
+      await axios.post("quizes.json", this.state.quiz);
       this.setState({
         quiz: [],
         rightAnswerId: 1,
