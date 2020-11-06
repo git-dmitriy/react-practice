724754cfda8174a1c8e510b8eba2fdb854de68f6
Dmitriy <shalberkind@mail.ru>
2020-11-06 16:41:36 +0300

    Загрузка списка тестов

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,16 +1,42 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import classes from "./QuizList.module.css";
-
+import axios from "axios";
 export default class QuizList extends Component {
+  state = {
+    quizes: [],
+  };
+
   renderQuizes() {
-    return [1, 2, 3].map((quiz, index) => {
+    return this.state.quizes.map((quiz) => {
       return (
-        <li key={index}>
-          <NavLink to={`/quiz/${quiz}`}>Тест {quiz}</NavLink>
+        <li key={quiz.id}>
+          <NavLink to={`/quiz/${quiz.id}`}>{quiz.name}</NavLink>
         </li>
       );
     });
+  }
+
+  async componentDidMount() {
+    try {
+      const response = await axios.get(
+        "https://react-tests-30d50.firebaseio.com/quizes.json"
+      );
+
+      const quizes = [];
+      Object.keys(response.data).forEach((key, index) => {
+        quizes.push({
+          id: key,
+          name: `Тест №${index + 1}`,
+        });
+      });
+
+      this.setState({
+        quizes,
+      });
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   render() {
