5239352ced007c39b83e73c87309db5e2ef542be
Dmitriy <shalberkind@mail.ru>
2020-11-06 21:22:18 +0300

    Обработка событий авторизация и входа на backend

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import classes from "./Auth.module.css";
 import Button from "../../components/UI/Button/Button";
 import Input from "../../components/UI/Input/Input";
+import axios from "axios";
 
 function validateEmail(email) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -38,8 +39,42 @@ export default class Auth extends Component {
     },
   };
 
-  logInHandler = () => {};
-  signInHandler = () => {};
+  logInHandler = async () => {
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true,
+    };
+
+    try {
+      const response = await axios.post(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyD_RFW-SzrUaQgIbDyMtyyOigjEj1XsM8E",
+        authData
+      );
+      console.log(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  signUpHandler = async () => {
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true,
+    };
+
+    try {
+      const response = await axios.post(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD_RFW-SzrUaQgIbDyMtyyOigjEj1XsM8E",
+        authData
+      );
+      console.log(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   submitHandler = (event) => {
     event.preventDefault();
   };
@@ -124,7 +159,7 @@ export default class Auth extends Component {
             </Button>
             <Button
               type="primary"
-              onClick={this.signInHandler}
+              onClick={this.signUpHandler}
               disabled={!this.state.isFormValid}
             >
               Регистрация
